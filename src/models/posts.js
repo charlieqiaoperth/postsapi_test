bfdf6054b5d7947bfa645c6b1451acd29421106e
silverwind <charlieqiaoperth@gmail.com>
2019-04-22 18:06:48 +0800

    model_posts done

---
+++ src/models/posts.js
@@ -0,0 +1,45 @@
+const posts=[];
+let currentId = 1;
+
+function addPost(post) {
+    const newPost = {...post,id: currentId++};
+    posts.push(newPost);
+    return newPost;
+}
+
+
+function getAllPost() {
+    return JSON.parse(JSON.stringify(post));
+}
+
+function getPostById(id) {
+    return posts.find(i => i.id===id);
+}
+
+function updatePostById(id,newPost) {
+    const postIndex=getPostIndexById(id);
+    // const post=posts[postIndex];
+    // post.author=newPost.author;
+    // post.name=newPost.name;
+    const updatePost={...newPost,id};
+    posts[postIndex]=updatePost;
+    return updatePost;
+}
+
+function deletePostById(id) {
+    const deletePostIndex=getPostIndexById(id);
+    const deletePost=posts.splice(deletePostIndex,1);
+    return deletePost;
+}
+
+function getPostIndexById(id) {
+    return posts.findIndex(i =>i.id===id)
+}
+
+module.exports = {
+    addPost,
+    getAllPost,
+    getPostById,
+    updatePostById,
+    deletePostById
+}
