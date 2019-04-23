7005461cf1cc1e2254c6a7a1c4ededac8b4eb80b
silverwind <charlieqiaoperth@gmail.com>
2019-04-23 23:04:21 +0800

    add some useful packages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 require('dotenv').config();
+const helmet = require('helmet');//保护框架信息和版本信息
+const morgan = require('morgan');
+const cors =  require('cors');//支持跨域请求
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.json()); 
+const morganLog = 
+    process.eventNames.NODE_ENV ==='production' ? morgan('common') : morgan('dev');
+app.use(helmet());
+app.use(morganLog);
+app.use(cors());
+
+
+app.use(express.json()); //从body取得json数据
 
 app.use('/v1', routes);
 
